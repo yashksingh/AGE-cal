7fff6d02419dae12e240be40cef718662d66bbb7
yashksingh <46723872+yashksingh@users.noreply.github.com>
2019-06-14 14:34:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var age=prompt("what is ur age?");
 alert("ur age is " +  age );
 var days = age * 365.25;
-alert( age + " years is roughly equal to " + days );
+alert( age + " years is roughly equal to " + days + "days" );
